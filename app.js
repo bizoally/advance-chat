78c73f713619e2b58c1e6834df92e5fc3ed4be27
BizOAlly <93839871+bizoally@users.noreply.github.com>
2022-08-28 19:30:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,90 @@
+$(window).load(function checkCookie() {
+  let user = getCookie("name");
+  if (user != "") {
+    $('.name').addClass("hide");
+    $('#namesubmit').addClass("hide");
+    $('#messageInput').removeClass("hide");
+    $('#msgsubmit').removeClass("hide");
+  } else {
+    $('#namesubmit').click(() =>{
+  
+      var name = $('#nameInput').val();
+              if (name != null){
+                setCookie("name", name, 30);
+                $('.name').addClass("hide");
+                $('#namesubmit').addClass("hide");
+                $('#messageInput').removeClass("hide");
+                $('#msgsubmit').removeClass("hide");
+              }
+    
+            });
+  }
+});
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+
+
+/*******************************************
+ ****************** main js ****************/
+
+
+var myDataRef = firebase.database().ref('chat');  
+
+       
+          $('#msgsubmit').click(()=> {
+            if ($('#messageInput').val().length >=1){
+           
+            var name = getCookie("name");
+            var text = $('#messageInput').val();
+           var time= new Date().getTime();
+            myDataRef.push({
+                name: name,
+                text: text,
+                time: time
+            });
+            $('#messageInput').val(''); 
+          }
+        });
+        
+    
+      myDataRef.on('child_added', function(chatting) {
+        var message = chatting.val();
+        
+        displayChatMessage(message.name, message.text);
+      });
+    
+      
+      function displayChatMessage(name, text) {
+        
+        $('<div/>').text(text).addClass("text-msg "+ name).prepend($('<em/>').text('~'+name).addClass("text-name "+ name)).appendTo($('#messagesDiv'));
+       
+       $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+        var findedname=getCookie("name");
+     $(".text-name."+findedname).addClass("self-name");
+     $(".text-msg."+findedname).addClass("self-msg");
+       
+      };
+
+    
+  
